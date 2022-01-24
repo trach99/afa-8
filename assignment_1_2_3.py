3367e2b705d2c8482eb3dca46d1e70292ec169a5
trach99 <austin@machine.localdomain>
2022-01-24 18:44:51 +0530

    remaining two

---
+++ assignment_1_2_3.py
@@ -0,0 +1,7 @@
+import numpy as np
+a = np.array([10,20,30]) 
+b = np.array([3,5,7])
+print("Power function: " + str(np.power(a,b)))
+print("Mod function: " + str(np.mod(a,b)))
+print("Remainder function: " + str(np.remainder(a,b)))
+print("Division function: " + str(np.floor_divide(a,b)))
